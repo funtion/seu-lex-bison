1f479e1863c7d4e0a16bcb849efe39b81fdba1a9
cz717 <huajianye630@gmail.com>
2015-05-28 16:57:26 +0800

    change dfa implement

--- seu-lex/seu-lex/dfa.h
+++ seu-lex/seu-lex/dfa.h
@@ -5,40 +5,32 @@
 #ifndef _DFA_H
 #define _DFA_H
 
-typedef unsigned int DFA_STATE_ID;
+#define CHAR_NUM 128
+#define MAX_DFA_STATE 1024
 
-struct DFA_EDGE
+typedef int DFA_STATE_ID;
+typedef DFA_STATE_ID *DFA_STATE;
+typedef DFA_STATE DFA_TABLE[MAX_DFA_STATE];
+
+struct DFA
 {
-	char c;
-	DFA_STATE_ID adjvex;
-	
-	DFA_EDGE(char ch, NFA_STATE_ID id) {
-		c = ch;
-		adjvex = id;
-	}
-};
-
-typedef list<DFA_EDGE> DFA_STATE;
-
-class DFA
-{
-private:
-	vector<DFA_STATE> states;
 	DFA_STATE_ID id;
-	set<DFA_STATE_ID> accept;
-	
-	void add_states();
-	void add_edge(DFA_STATE_ID begin, char c, DFA_STATE_ID end);
-	
-public:
-	DFA();
-	~DFA();
-
-	void nfa_to_dfa(NFA_TABLE table, NFA nfa);
-	void dfa_mini();
-	void dfa_write(FILE* file);
-	void print_dfa();
+	DFA_TABLE tbl;
 };
+
+void dfa_state_ini(DFA_STATE state);
+void dfa_ini(DFA &dfa);
+void dfa_clear(DFA &dfa);
+
+DFA_STATE_ID dfa_add_states(DFA &dfa);
+void dfa_add_edge(DFA_TABLE dfa_tbl, DFA_STATE_ID begin, char c, DFA_STATE_ID end);
+
+void nfa_to_dfa(NFA_TABLE nfa_tbl, NFA nfa, DFA &dfa);
+void dfa_mini(DFA &dfa);
+void dfa_write(DFA &dfa, FILE* file);
+
+void print_dfa(DFA &dfa);
+
 
 
 
